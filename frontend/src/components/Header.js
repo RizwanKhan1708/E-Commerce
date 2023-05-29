806b6c998711df13094d3a3c13a24fd38a7b61f1
RizwanKhan1708 <rizwankhan001708@gmail.com>
2023-05-30 04:51:15 +0500

    Logo issue solved

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ function Header(){
     return(
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
         <div className="container">
-            <Link className="navbar-brand" to="/">PR Market place</Link>
+            <Link className="navbar-brand" to="/">Valuefy Market Place</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
