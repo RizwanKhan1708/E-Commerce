0d2c752a8595b3335ef07ea19fdb7bc90f9849e9
RizwanKhan1708 <rizwankhan001708@gmail.com>
2023-05-29 17:18:47 +0500

    Updated single product page frontend

--- frontend/src/components/SingleProduct.js
+++ frontend/src/components/SingleProduct.js
@@ -1,12 +1,13 @@
 import logo from "../logo.svg";
+import {Link} from "react-router-dom";
 
 function SingleProduct(props){
     return(
         <div className='col-12 col-md-3 mb-4'>
             <div className="card shadow" >
-                <img src={logo} className="card-img-top" alt="..."/>
+                <Link to="/product/python-timer/123"><img src={logo} className="card-img-top" alt="..."/></Link>
                 <div className="card-body">
-                    <h4 className="card-title">{props.title}</h4>
+                    <h4 className="card-title"><Link to="/product/python-timer/123">{props.title}</Link></h4>
                     <h5 className="card-title text-muted">Price Rs. 500</h5>
                 </div>
                 <div className='card-footer'>
